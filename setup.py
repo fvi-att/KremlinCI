f019287ba53399019a0340526de7472bc1882bb7
shimizu <jshimizujp@gmail.com>
2018-03-13 13:53:59 +0900

    [feature] add setup.py for install

---
+++ setup.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+from setuptools import setup
+
+
+def main():
+   setup(
+       name='kremlinCI',
+       version='0.0.1',
+       zip_safe=False,
+       packages=[],
+       install_requires=[],
+       dependency_links=[]
+   )
+
+
+if __name__ == '__main__':
+   main()
